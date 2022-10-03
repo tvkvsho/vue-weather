21f5bef54544a9851dbdfce88f6370eff7bfffb3
IGOR <l3monad@gmail.com>
2022-10-03 15:07:56 +0300

    vue-weather-ready-v1.5-fixed-skeleton

--- src/components/CityViewSkeleton.vue
+++ src/components/CityViewSkeleton.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col flex-1">
+  <div class="city-view-skeleton">
     <!-- Overview -->
     <div class="flex flex-col py-12 items-center">
       <AnimatedPlaceholder class="max-w-[300px] w-full mb-2" />
@@ -12,17 +12,19 @@
         class="max-w-[300px] h-[75px] w-full"
       />
     </div>
-    <!-- Hourly -->
-    <div class="flex flex-col py-12 px-8 items-center">
-      <AnimatedPlaceholder
-        class="max-w-screen-md h-[100px] w-full mb-12 "
-      />
-    </div>
-    <!-- Weekly -->
-    <div class="flex flex-col py-12 px-8 items-center">
-      <AnimatedPlaceholder
-        class="max-w-screen-md h-[100px] w-full mb-12"
-      />
+    <div>
+      <!-- Hourly -->
+      <div class="flex flex-col py-12 px-8 items-center">
+        <AnimatedPlaceholder
+            class="max-w-screen-md h-[100px] w-full mb-12 "
+        />
+      </div>
+      <!-- Weekly -->
+      <div class="flex flex-col py-12 px-8 items-center">
+        <AnimatedPlaceholder
+            class="max-w-screen-md h-[100px] w-full mb-12"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -30,3 +32,17 @@
 <script setup>
 import AnimatedPlaceholder from "./AnimatedPlaceholder.vue";
 </script>
+
+<style lang="scss">
+  .city-view-skeleton{
+    display: grid;
+    grid-template-columns: 30% 67%;
+    gap: 40px;
+    padding-bottom: 40px;
+
+    @media (max-width: 768px) {
+      display: flex;
+      flex-direction: column;
+    }
+  }
+</style>
